78cf710d621854da68e00b2edfb9fbd8b161525f
Anurag <anurag@itriangle.in>
2026-02-06 14:42:56 +0530

    Fix : Fixed redis bootstrap close function

--- src/bootstrap/redis.js
+++ src/bootstrap/redis.js
@@ -1,42 +1,9 @@
-// const { redisClient } = require('../infrastructure/redis');
-// const logger = require('../infrastructure/logger');
-
-// /**
-//  * Initialize Redis connection
-//  */
-// const initializeRedis = async () => {
-//   try {
-//     if (!redisClient.isReady) {
-//       await redisClient.connect();
-//     }
-//     logger.info('Redis initialized successfully');
-//     return redisClient;
-//   } catch (error) {
-//     logger.error('Failed to initialize Redis:', error);
-//     throw error;
-//   }
-// };
-
-// /**
-//  * Close Redis connection
-//  */
-// const closeRedis = async () => {
-//   try {
-//     await redisClient.quit();
-//     logger.info('Redis connection closed');
-//   } catch (error) {
-//     logger.error('Error closing Redis:', error);
-//   }
-// };
-
-// module.exports = {
-//   initializeRedis,
-//   closeRedis,
-// };
-
 const redisClient = require('../infrastructure/redis/client');
 const logger = require('../infrastructure/logger');
 
+/**
+ * Initialize Redis connection
+ */
 const initializeRedis = async () => {
   try {
     if (!redisClient.isOpen) {
@@ -49,6 +16,19 @@ const initializeRedis = async () => {
   }
 };
 
+/**
+ * Close Redis connection
+ */
+const closeRedis = async () => {
+  try {
+    await redisClient.quit();
+    logger.info('Redis connection closed');
+  } catch (error) {
+    logger.error('Error closing Redis:', error);
+  }
+};
+
 module.exports = {
   initializeRedis,
+  closeRedis
 };
